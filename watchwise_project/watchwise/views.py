18b020d725bda6ec1c3c7300655ac38275a5efe9
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2024-03-21 21:13:23 +0100

    Edited views.py and renamed common.html

--- watchwise_project/watchwise/views.py
+++ watchwise_project/watchwise/views.py
@@ -1,39 +1,27 @@
-from django.http import HttpResponse
-from django.template import loader
-from .models import Media, Type
+from django.shortcuts import render
+from .models import Media
 
 def watchwise(request):
-    template = loader.get_template('homepage.html')
-    context = {}
-    return HttpResponse(template.render(context=context))
+    return render(request, 'homepage.html')
 
 def movies(request):
     movies = Media.objects.filter(media_type=1)
-
-    template = loader.get_template('movies.html')
     context = {
         'movies': movies,
     }
+    return render(request, 'movies.html', context)
 
-    return HttpResponse(template.render(context=context))
 
 def tv_shows(request):
     tv_shows = Media.objects.filter(media_type=2)
-
-    template = loader.get_template('tv_shows.html')
     context = {
         'tv_shows': tv_shows,
     }
-
-    return HttpResponse(template.render(context=context))
+    return render(request, 'tv_shows.html', context)
 
 def anime(request):
     anime = Media.objects.filter(media_type=3)
-
-    template = loader.get_template('anime.html')
     context = {
         'anime': anime,
     }
-
-    return HttpResponse(template.render(context=context))
-
+    return render(request, 'anime.html', context)
