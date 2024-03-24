c69bda8d528f143e11cd71ab817f043d230ba248
Lorenzo Lucchesi <lorenzolucchesi3@gmail.com>
2024-03-24 17:07:03 +0100

    Added TMDB API and updated the project

--- watchwise_project/watchwise/urls.py
+++ watchwise_project/watchwise/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.watchwise, name='homepage'),
     path('movie_list', views.movie_list, name='movie_list'),
     path('tv_show_list', views.tv_show_list, name='tv_show_list'),
+    path('results', views.results, name='results'),
 ]
--- watchwise_project/watchwise/views.py
+++ watchwise_project/watchwise/views.py
@@ -1,8 +1,47 @@
-from django.shortcuts import render
 from .models import Media
+from django.shortcuts import render
+import requests
+from django.http import HttpResponse
+from datetime import datetime
+
+# Define the API key for The Movie Database
+TMDB_API_KEY = "05e5be7a518e07b0cdd93bf0e133083a"
 
 def watchwise(request):
     return render(request, 'homepage.html')
+
+# View for the homepage
+def watchwise(request):
+    # Render the homepage template
+    return render(request, 'homepage.html')
+
+# View for movies
+def results(request):
+    # Get the query from the search box
+    query = request.GET.get('q')
+
+    # If the query is not empty
+    if query:
+        # Get the results from the API
+        data = requests.get(f"https://api.themoviedb.org/3/search/{request.GET.get('type')}?api_key={TMDB_API_KEY}&language=en-US&page=1&include_adult=false&query={query}")
+        data = data.json()
+
+        # Convert release_date and first_air_date to just the year
+        for m in data['results']:
+            if 'release_date' in m and m['release_date']:
+                m['release_date'] = datetime.strptime(m['release_date'], '%Y-%m-%d').year
+            if 'first_air_date' in m and m['first_air_date']:
+                m['first_air_date'] = datetime.strptime(m['first_air_date'], '%Y-%m-%d').year
+
+    else:
+        # If the query is empty, return a message
+        return HttpResponse("Please enter a search query")
+
+    # Render the movies template with the data from the API
+    return render(request, 'results.html', {
+        "data": data,
+        "type": request.GET.get("type")
+    })
 
 def movie_list(request):
     movies = Media.objects.filter(media_type=1)
